a9681ca64f2b3627175ee146b3cb48c6ef2ade62
Unknown <anthonydotnet@gmail.com>
2019-03-24 00:22:50 +0000

    Opinionated interface

---
+++ src/DangEasy.Interfaces.Caching/ICache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DangEasy.Interfaces.Caching
+{
+    public interface ICache
+    {
+        bool Add(string key, object value, int? cacheSeconds = null);
+        T Get<T>(string key);
+        T Get<T>(string cacheKey, Func<T> hydrationFunction, int? cacheSeconds = null) where T : class;
+        bool Remove(string key);
+        void RemoveByPrefix(string cachePrefix);
+    }
+}
